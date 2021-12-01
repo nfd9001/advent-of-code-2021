dcaa81e0e394b057dd700b5fcc7fcb32b664f92e
nfd (aka alex) <github@nfd.moe>
2021-11-30 21:48:58 -0800

    Add day 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aaaah
+I forgot to prep anything for this year;
+proper readme incoming.
+Going to do Haskell again, as is my wont.
+
---
+++ day1.hs
@@ -0,0 +1,10 @@
+pairs l = zip l $ tail l
+getWindows (x:y:z:xs) = (x + y + z):getWindows (y:z:xs)
+getWindows _ = []
+
+main = do
+  f     <- readFile "inputs/day1.txt"
+  let ns = (read <$> lines f) :: [Integer]
+  print $ length $ filter (uncurry (<)) $ pairs ns
+  print $ length $ filter (uncurry (<)) $ pairs $ getWindows ns
+
